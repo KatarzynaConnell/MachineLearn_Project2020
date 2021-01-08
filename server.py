401a5654750051e5447e381c8ae9aabe0c1b95a6
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2021-01-08 20:22:47 +0000

    updated the server.py code in attempt to connect to the static file

--- server.py
+++ server.py
@@ -1,22 +1,45 @@
 # flask for web app.
-import flask as fl
+import flask
+from flask import Flask, request, jsonify
 # numpy for numerical work.
+import requests
+import joblib
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import pickle
+
+# load the model
+with open('polynomial_model.pkl', 'rb') as file:  
+    Pickled_polyn_model = pickle.load(file)
+
+Pickled_LR_Model
+
 
 # Create a new web app.
-app = fl.Flask(__name__)
+app = Flask(__name__)
 
-# Add root route.
+# Add root route for the home page.
 @app.route("/")
 def home():
-  return app.send_static_file('index.html')
 
-# Add uniform route (json)
-@app.route('/api/uniform')
-def uniform():
-  return {"value": np.random.uniform()}
+# Add polynomial regression model (json)
+@app.route('polynomial_model', method=['POST'])
+def polynomial_model():
+    wind = request.form['input']
+# for wind speeds that are less than 0.3 and above 24.499 return 0 
+        if wind > 24.5:  
+            power = 0
+        return jsonify({'name' : float(power)})
+    
+  # pass to model for wind speeds between 0.3 and 24.499	
+        if 0.3 < wind < 24.5:
+            power = polyn_model.predict(polyn_features.fit_transform([[wind]]))
+            power = power.item(0)
+        return jsonify({'name' : float(power)})
+ 
+        if wind < 0.3: 
+            print("Missing data!")
 
-# Add normal route (json)
-@app.route('/api/normal')
-def normal():
-  return {"value": np.random.normal()}
+if __name__ == '__main__':
+    app.run(debug = True)
